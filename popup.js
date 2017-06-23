bf15dee7ecad18c269dced95458a7212b0224380
mzpbvsig <972403325@qq.com>
2017-06-23 12:26:11 +0800

    format

--- popup.js
+++ popup.js
@@ -1,96 +1,92 @@
-$(function(){
-  getValueByArray(["username", "bumen", "noon_auto", "night_auto"], function(result){
-      if(result.username != undefined){
-         $(".username").val(result.username);
-      }
+$(function() {
+    getValueByArray(["username", "bumen", "noon_auto", "night_auto", "noon_time", "night_time"],
+    function(result) {
+        if (result.username != undefined) {
+            $(".username").val(result.username);
+        }
 
-      if(result.bumen != undefined ){
-         $(".bumen").val(result.bumen);
-      }
+        if (result.bumen != undefined) {
+            $(".bumen").val(result.bumen);
+        }
 
-      if(result.noon_auto != undefined){
-         if(result.noon_auto){
-            $(".noon_auto").prop("checked", "checked");
-         }
-      }
+        if (result.noon_auto != undefined) {
+            if (result.noon_auto) {
+                $(".noon_auto").prop("checked", "checked");
+            }
+        }
 
-      if(result.night_auto != undefined){
-         if(result.night_auto){
-            $(".night_auto").prop("checked", "checked");
-         }
-      }
-  });
+        if (result.night_auto != undefined) {
+            if (result.night_auto) {
+                $(".night_auto").prop("checked", "checked");
+            }
+        }
 
+        if (result.noon_time != undefined) {
+            $(".noon_time").val(result.noon_time);
+        }
 
-  $(".save").click(function(){
-      username = $(".username").val();
-      bumen = $(".bumen").val();
-      noon_auto = $(".noon_auto").prop("checked");
-      night_auto = $(".night_auto").prop("checked");
-      saveObject({
-        "username": username,
-        "bumen": bumen,
-        "noon_auto": noon_auto,
-        "night_auto": night_auto
-      }, function(){
-          $(".message").html("保存成功");
-          notification("message", "提示",  "保存成功");
-          clear();
-      });
-  });
-
-
-  $(".hand2").click(function(){
-        noon2(function(){
-            $(".message").html("午餐点成功");
-            notification("message", "点餐提示",  "午餐点成功");
-            clear();
-        }, function(){
-            $(".message").html("抱歉,不在点餐时间");
-            notification("message", "抱歉",  "抱歉,不在点餐时间");
-            clear();
-        });
-        night2(function(){
-            $(".message").html("晚餐点成功");
-            notification("message", "点餐提示",  "晚餐点成功");
-            clear();
-        }, function(){
-            $(".message").html("抱歉,不在点餐时间");
-            notification("message", "抱歉",  "抱歉,不在点餐时间");
-            clear();
-        });
-  });
-
-
-  $(".view").click(function(){
-
-    getValueByArray(["username"], function(result){
-      view(result.username, function(){}, function(data){
-          $(".session").hide();
-          $(".session2").show();
-          jdata = $(data);
-          trs = jdata.find(".detail_list tr");
-          for(var i = 0 ; i < trs.size() ; i++){
-              tr = trs.eq(i);
-              if(tr.html().indexOf(result.username) != -1){
-                 tr.addClass("redBg");
-                 break;
-              }
-          }
-          $(".whos").html(jdata.find(".detail_list").html());
-      });
+        if (result.night_time != undefined) {
+            $(".night_time").val(result.night_time);
+        }
     });
-  });
 
-  $(".back").click(function(){
-      $(".session").show();
-      $(".session2").hide();
-  });
+    $(".save").click(function() {
+        username = $(".username").val();
+        bumen = $(".bumen").val();
+        noon_time = $(".noon_time").val();
+        night_time = $(".night_time").val();
+        noon_auto = $(".noon_auto").prop("checked");
+        night_auto = $(".night_auto").prop("checked");
 
-  function clear(){
-    var ticker = setTimeout(function(){
-          $(".message").html("");
-          clearTimeout(ticker);
-    }, 8000);
-  }
+        saveObject({
+            "username": username,
+            "bumen": bumen,
+            "noon_auto": noon_auto,
+            "night_auto": night_auto,
+            "noon_time": noon_time,
+            "night_time": night_time
+        },
+        function() {
+            message("温馨提醒", "保存成功");
+        });
+    });
+
+    $(".noon_hand").click(function() {
+        noon2();
+    });
+
+    $(".night_hand").click(function() {
+        night2();
+    });
+
+    $(".view").click(function() {
+        getValueByArray(["username"],
+        function(result) {
+            view(result.username,
+            function() {},
+            function(data) {
+                jdata = $(data);
+                trs = jdata.find(".detail_list tr");
+                if(trs.size() <= 1){
+                    message("温馨提醒", "抱歉, 暂时还没有人员点餐。");
+                    return;
+                }
+                $(".session").hide();
+                $(".session2").show();
+                for (var i = 0; i < trs.size(); i++) {
+                    tr = trs.eq(i);
+                    if (tr.html().indexOf(result.username) != -1) {
+                        tr.addClass("redBg");
+                        break;
+                    }
+                }
+                $(".whos").html(jdata.find(".detail_list").html());
+            });
+        });
+    });
+
+    $(".back").click(function() {
+        $(".session").show();
+        $(".session2").hide();
+    });
 });
--- util.js
+++ util.js
@@ -1,81 +1,96 @@
-function login(username, password,  callback){
-  var cookieId = 'PHPSESSID';
-  $.ajax({
-      type: 'POST',
-      url: "http://dc.wk2.com/Home/User/login.html",
-      data:{"username": username, "password": password},
-      crossDomain: true,
-      success: function(data, status, xhr) {
-        chrome.cookies.get({"url": "http://dc.wk2.com/Home/User/login.html", "name": cookieId}, function(cookie) {
-             var cookieStr = cookieId + "=" + cookie.value;
-             callback(cookieStr);
-        });
-      },
-      cache: false
-  });
-}
-
-function view(username, success, show){
-  var loginusername = "张凯";
-  var password = "123456";
-
-  login(loginusername, password, function(cookieStr){
+function login(username, password, callback) {
+    var cookieId = 'PHPSESSID';
     $.ajax({
-        type: 'GET',
-        url: "http://dc.wk2.com/Home/Index/index.html",
+        type: 'POST',
+        url: "http://dc.wk2.com/Home/User/login.html",
+        data: {
+            "username": username,
+            "password": password
+        },
         crossDomain: true,
         success: function(data, status, xhr) {
-            if(data.indexOf(username) != -1){
-                success(data);
-            }
-            if(show){
-              show(data);
-            }
+            chrome.cookies.get({
+                "url": "http://dc.wk2.com/Home/User/login.html",
+                "name": cookieId
+            },
+            function(cookie) {
+                var cookieStr = cookieId + "=" + cookie.value;
+                callback(cookieStr);
+            });
+        },
+        cache: false
+    });
+}
+
+function view(username, successfn, showfn) {
+    var loginusername = "张凯";
+    var password = "123456";
+
+    login(loginusername, password,
+    function(cookieStr) {
+        $.ajax({
+            type: 'GET',
+            url: "http://dc.wk2.com/Home/Index/index.html",
+            crossDomain: true,
+            success: function(data, status, xhr) {
+                if (data.indexOf(username) != -1) {
+                    successfn(data);
+                }
+                if (showfn) {
+                    showfn(data);
+                }
+            },
+            // beforeSend: function(xhr){xhr.setRequestHeader('Cookie', cookieStr);},
+            cache: false
+        });
+    });
+
+}
+
+function order(username, bumen, o_type, cookieStr, successfn) {
+    $.ajax({
+        type: 'POST',
+        url: "http://dc.wk2.com/Home/User/order.html",
+        data: {
+            'name': username,
+            'bumen': bumen,
+            'o_type': o_type
+        },
+        crossDomain: true,
+        success: function(data, status, xhr) {
+            view(username,
+            function(data) {
+                saveObject({
+                    "success": o_type
+                },
+                function() {
+                    successfn(username, o_type);
+                });
+            });
         },
         // beforeSend: function(xhr){xhr.setRequestHeader('Cookie', cookieStr);},
         cache: false
     });
-  });
-
 }
 
-function order(username, bumen, o_type, cookieStr, success){
-  $.ajax({
-      type: 'POST',
-      url: "http://dc.wk2.com/Home/User/order.html",
-      data : {'name' : username,
-              'bumen' : bumen,
-              'o_type' : o_type
-      },
-      crossDomain: true,
-      success: function(data, status, xhr) {
-         view(username, function(data){
-              saveObject({"success": o_type}, function(){
-                    success(username, o_type);
-              });
-         });
-      },
-      // beforeSend: function(xhr){xhr.setRequestHeader('Cookie', cookieStr);},
-      cache: false
-  });
+function order2(o_type, successfn) {
+    var username = "张凯",
+    password = "123456";
+
+    login(username, password,
+    function(cookieStr) {
+        getValue(["username", "bumen"],
+        function(result) {
+            order(result.username, result.bumen, o_type, cookieStr, successfn);
+        });
+    });
 }
 
-function order2(o_type, success){
-  var username = "张凯",
-      password = "123456";
-
-  login(username, password, function(cookieStr){
-      getValue(["username", "bumen"], function(result){
-          order(result.username, result.bumen, o_type, cookieStr, success);
-      });
-  });
-}
-
-function isWeekDay(){
+function isWeekDay() {
     var date = new Date();
     var day = date.getDay();
-    if(day == 6 || day == 0){
-       return false;
+    if (day == 6 || day == 0) {
+        return false;
     }
     return true;
 }
@@ -83,103 +98,120 @@ function isWeekDay(){
 function checkHour(hour) {
     var date = new Date();
     var h = date.getHours();
-    var m = date.getMinutes();
 
-    if(hour == h){
-      return true;
+    if (hour == h) {
+        return true;
     }
 
     return false;
 }
 
-function noon(){
-   noon2();
-   console.log("noon2 run");
+function noon() {
+    console.log("auto noon run");
+    if (checkHour('09') || checkHour('9')) {
+        getValue(["success", "noon_time"],
+        function(result) {
+            var date = new Date();
+            var minutes = date.getMinutes();
+            if (minutes >= parseInt(result.noon_time) && result.success != 1) {
+                order2(1,
+                function(username, o_type) {
+                    notification("noon", "温馨提醒", username + "：已为您点好午餐.");
+                });
+            }
+        });
+    }
 }
 
-function noon2(callback, ggcallback){
-   if(checkHour('09') || checkHour('9')){
-     getValue(["success"], function(result){
-        if(result.success != 1){
-          order2(1, function(username, o_type){
-              notification("noon", "点餐提示", username + "：已为您点好午餐.");
-          });
-        } else {
-          if(callback){
-              callback();
-          }
-        }
-     });
-   } else {
-     if(ggcallback){
-         ggcallback();
-     }
-   }
+function noon2() {
+    if (checkHour('09') || checkHour('9')) {
+        order2(1,
+        function(username, o_type) {
+            notification("noon", "温馨提醒", username + "：已为您点好午餐.");
+        });
+        return;
+    }
+    message("温馨提醒", "抱歉,不在点餐时间");
 }
 
-function night2(callback, ggcallback){
-   if(checkHour('14') || checkHour('15')){
-     getValue("success", function(result){
-        if(result.success != 2){
-            order2(2, function(username, o_type){
-               notification("night", "点餐提示", username + "：已为您点好晚餐.");
-            });
-        } else{
-          if(callback){
-              callback();
-          }
-        }
-     });
-   } else {
-     if(ggcallback){
-         ggcallback();
-     }
-   }
+function night2() {
+    if (checkHour('14') || checkHour('15')) {
+        order2(2,
+        function(username, o_type) {
+            notification("night", "点餐提示", username + "：已为您点好晚餐.");
+        });
+    }
+    message("温馨提醒", "抱歉,不在点餐时间");
 }
 
-
-function night(){
-   night2();
-   console.log("night2 run");
+function night() {
+    console.log("auto night run");
+    if (checkHour('14') || checkHour('15')) {
+        getValue(["success", "night_time"],
+        function(result) {
+            var date = new Date();
+            var minutes = date.getMinutes();
+            var hour = date.getHours();
+            if (minutes >= parseInt(result.night_time) && result.success != 2) {
+                order2(2,
+                function(username, o_type) {
+                    notification("night", "点餐提示", username + "：已为您点好晚餐.");
+                });
+            }
+        });
+    }
 }
 
-function offwork(){
-   if(checkHour('19')){
-     var date = new Date();
-     var d = date.getDay();
-     getValue("offwork", function(result){
-          if(result.offwork != d){
-              notification("offwork", "用餐提示", "晚餐时间到请注意用餐！");
-              saveObject({"offwork" : d}, function(){});
-          }
-     });
-   }
-   console.log("offwork run");
+function offwork() {
+    if (checkHour('19')) {
+        var date = new Date();
+        var d = date.getDay();
+        getValue("offwork",
+        function(result) {
+            if (result.offwork != d) {
+                notification("offwork", "用餐提示", "晚餐时间到请注意用餐！");
+                saveObject({
+                    "offwork": d
+                },
+                function() {});
+            }
+        });
+    }
+    console.log("offwork run");
 }
 
-function saveObject(obj, callback){
-  chrome.storage.local.set(obj, callback);
+function saveObject(obj, callback) {
+    chrome.storage.local.set(obj, callback);
 }
 
-function getValue(key, callback){
-  chrome.storage.local.get(key, callback);
+function getValue(key, callback) {
+    chrome.storage.local.get(key, callback);
 }
 
-function getValueByArray(array, callback){
-  chrome.storage.local.get(array, callback);
+function getValueByArray(array, callback) {
+    chrome.storage.local.get(array, callback);
 }
 
-
-function notification(id, title, message){
-  chrome.notifications.create(
-    id,{
-    type: 'basic',
-    iconUrl: 'icon128.png',
-    title: title,
-    message: message
+function notification(id, title, message) {
+    chrome.notifications.create(id, {
+        type: 'basic',
+        iconUrl: 'icon128.png',
+        title: title,
+        message: message
     },
+    function() {});
+}
 
-function() {}
+function clear() {
+    var ticker = setTimeout(function() {
+        $(".message").html("<div class=\"dc_notice\">" + "1、点餐时间，中餐 9:00-10:00， 晚餐14：00-15:30<br>" + "2、周六周日暂时不提供午晚餐<br>" + "3、取消点餐，请联系人事部<br>" + "4、好米、好油、好菜、好厨艺；乐厨让你吃饭无忧<br><br></div>");
+        clearTimeout(ticker);
+    },
+    8000);
+}
 
-);
+function message(title, msg) {
+    $(".message").html(msg);
+    clear();
+    notification("message", title, msg);
 }
